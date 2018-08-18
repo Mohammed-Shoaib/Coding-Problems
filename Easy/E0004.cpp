7e99de715ccabafbb1c5f9190d51e63860edaf95
Mohammed Shoaib <32389312+mohammed-shoaib@users.noreply.github.com>
2018-08-19 01:43:37 +0400

    Add files via upload

---
+++ Easy/E0004.cpp
@@ -0,0 +1,28 @@
+/*
+Link: https://www.hackerrank.com/challenges/a-very-big-sum/problem
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+long aVeryBigSum(vector<long> arr) {
+    long sum = 0;
+    for(int num: arr)
+        sum += num;
+    return sum;
+}
+
+int main()
+{
+    int len, num;
+    cin>>len;
+    vector<long> arr(len);
+    for(int i=0 ; i<len ; i++){
+        cin>>num;
+        arr.push_back(num);
+    }
+    cout<<aVeryBigSum(arr);
+    return 0;
+}
