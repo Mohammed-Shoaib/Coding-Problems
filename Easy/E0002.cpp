a293420f05afa83f50aee3abb4db1f7515ff2550
Mohammed Shoaib <32389312+mohammed-shoaib@users.noreply.github.com>
2018-08-19 01:37:03 +0400

    Add files via upload

---
+++ Easy/E0002.cpp
@@ -0,0 +1,28 @@
+/*
+Link: https://www.hackerrank.com/challenges/simple-array-sum/problem
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int simpleArraySum(vector<int> arr) {
+    int sum = 0;
+    for(int i=0 ; i<arr.size() ; i++)
+        sum += arr[i];
+    return sum;
+}
+
+int main()
+{
+    int len, num;
+    cin>>len;
+    vector<int> arr;
+    for(int i=0 ; i<len ; i++){
+        cin>>num;
+        arr.push_back(num);
+    }
+    cout<<simpleArraySum(arr);
+    return 0;
+}
