1d368bb0878cbc240248e3c6a79cd3f963e5b2b7
Mohammed Shoaib <32389312+Mohammed-Shoaib@users.noreply.github.com>
2018-09-07 23:32:40 +0400

    Add files via upload

---
+++ Easy/E0019.cpp
@@ -0,0 +1,24 @@
+/*
+Problem Statement: https://www.hackerrank.com/challenges/utopian-tree/problem
+*/
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int utopianTree(int n) {
+    int num = pow(2,ceil(n/2.0)+1);
+    return (n&1)?(num-2):(num-1);
+}
+
+int main()
+{
+    int t,n;
+    cin>>t;
+    for(int i=1 ; i<=t ; i++){
+        cin>>n;
+        cout<<utopianTree(n)<<endl;
+    }
+    return 0;
+}
