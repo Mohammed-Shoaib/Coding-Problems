aa99f36ab4a24c40031de48a7116306681d4e735
Mohammed-Shoaib <shoaib98libra@gmail.com>
2020-04-06 12:21:52 +0400

    [LC] Update Group Anagrams

--- LeetCode/Solutions/LC0049.cpp
+++ LeetCode/Solutions/LC0049.cpp
@@ -4,20 +4,16 @@ Problem Statement: https://leetcode.com/problems/group-anagrams/
 
 class Solution {
 public:
-    vector< vector<string> > groupAnagrams(vector<string>& strs) {
-		string s;
-		vector< vector<string> > groups;
-		unordered_map<string, vector<string> > m;
-
-		for (string& word: strs) {
-			s = word;
-			sort(s.begin(), s.end());
-			m[s].push_back(word);
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+		vector<vector<string>> groups;
+        unordered_map<string, vector<string>> m;
+		for (string& s: strs) {
+			string copy(s);
+			sort(copy.begin(), copy.end());
+			m[copy].push_back(s);
 		}
-		
-		for (auto it: m)
-			groups.push_back(it.second);
-		
+		for (auto& [k, v]: m)
+			groups.push_back(v);
 		return groups;
     }
 };
