e8bda8a1c88494bbbc4932fc5c85fafe8419e2d7
Mohammed-Shoaib <shoaib98libra@gmail.com>
2020-05-29 01:16:58 +0400

    [LC] Update Longest Common Subsequence

--- LeetCode/Solutions/LC1143.cpp
+++ LeetCode/Solutions/LC1143.cpp
@@ -4,22 +4,21 @@ Problem Statement: https://leetcode.com/problems/longest-common-subsequence/
 
 class Solution {
 public:
-	int longestCommonSubsequence(string &text1, string &text2) {
-		int n, m;
-		n = text1.length();
-		m = text2.length();
-		vector<int> prev(m + 1), curr(m + 1);
+	int longestCommonSubsequence(string text1, string text2) {
+		int m, n;
+		m = text1.length();
+		n = text2.length();
+		vector<vector<int>> dp(m + 1, vector<int>(n + 1));
 
-		for (int i = 1; i <= n; i++) {
-			for (int j = 1; j <= m; j++) {
+		// dynamic programming
+		for (int i = 1; i <= m; i++)
+			for (int j = 1; j <= n; j++) {
 				if (text1[i - 1] == text2[j - 1])
-					curr[j] = prev[j - 1] + 1;
+					dp[i][j] = 1 + dp[i - 1][j - 1];
 				else
-					curr[j] = max(prev[j], curr[j - 1]);
+					dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
 			}
-			prev = curr;
-		}
-
-		return curr[m];
+		
+		return dp[m][n];
 	}
 };
