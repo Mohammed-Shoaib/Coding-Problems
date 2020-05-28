42b3259f1aa7232ab0dbd77e3926875e753c2b33
Mohammed-Shoaib <shoaib98libra@gmail.com>
2020-05-28 21:39:07 +0400

    [LC] Solve Check If a String Is a Valid Sequence from Root to Leaves Path in a Binary Tree

---
+++ LeetCode/Solutions/LC1430.cpp
@@ -0,0 +1,23 @@
+/*
+Problem Statement: https://leetcode.com/problems/check-if-a-string-is-a-valid-sequence-from-root-to-leaves-path-in-a-binary-tree/
+*/
+
+class Solution {
+public:
+	bool isValidSequence(TreeNode* root, vector<int>& arr) {
+		return isValidSequence(0, root, arr);
+	}
+
+	bool is_leaf(TreeNode* root) {
+		return root && !root->left && !root->right;
+	}
+
+	bool isValidSequence(int ptr, TreeNode* root, vector<int>& arr) {
+		if (!root || ptr == arr.size() || root->val != arr[ptr])
+			return false;
+		else if (ptr + 1 == arr.size())
+			return is_leaf(root);
+		else
+			return isValidSequence(ptr + 1, root->left, arr) || isValidSequence(ptr + 1, root->right, arr);
+	}
+};
--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Solutions to some of the coding problems from popular websites. Currently, there
 
 ## Stats
 
-Number of problems solved: 831
+Number of problems solved: 832
 
 ### HackerRank
 
@@ -33,7 +33,7 @@ Number of problems solved: 831
 | Type 		| Solved 	|
 |:---------:|:---------:|
 | Easy 		| 46 		|
-| Medium 	| 102 		|
+| Medium 	| 103 		|
 | Hard 		| 3 		|
 
 ### Google
