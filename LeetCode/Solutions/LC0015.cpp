1612f7f8b8740b6ab65b13f9635a875bf3ed5306
Mohammed-Shoaib <shoaib98libra@gmail.com>
2020-07-08 17:33:14 +0400

    [LC] Update 3Sum

--- LeetCode/Solutions/LC0015.cpp
+++ LeetCode/Solutions/LC0015.cpp
@@ -1,37 +1,39 @@
 /*
 Problem Statement: https://leetcode.com/problems/3sum/
+Time: O(n²)
+Space: O(n)
+Author: Mohammed Shoaib, github.com/Mohammed-Shoaib
 */
 
 class Solution {
 public:
-    vector< vector<int> > threeSum(vector<int> &nums) {
-    	int low, high, sum;
-    	vector< vector<int> > triplets;
-    	sort(nums.begin(), nums.end());
+	vector<vector<int>> threeSum(vector<int> nums) {
+		int l, r, sum, n = nums.size();
+		vector<vector<int>> triplets;
+		sort(nums.begin(), nums.end());
 
-    	for (int i = 0; i < (int) nums.size() - 2 && nums[i] <= 0; i++) {
-    		if (i > 0 && nums[i] == nums[i - 1])
-    			continue;
-    		low = i + 1;
-    		high = nums.size() - 1;
-    		while (low < high) {
-    			sum = nums[i] + nums[low] + nums[high];
-    			if (sum == 0) {
-    				triplets.push_back({nums[i], nums[low], nums[high]});
-    				while (low < high && nums[low] == nums[low + 1])
-    					low++;
-    				while (low < high && nums[high] == nums[high - 1])
-    					high--;
-    				low++;
-    				high--;
-    			}
-    			else if (sum < 0)
-    				low++;
-    			else
-    				high--;
-    		}
-    	}
-
-    	return triplets;
-    }
+		for (int i = 0; i < n && nums[i] <= 0; i++) {
+			if (i > 0 && nums[i] == nums[i - 1])
+				continue;
+			l = i + 1;
+			r = n - 1;
+			while (l < r) {
+				sum = nums[i] + nums[l] + nums[r];
+				if (sum == 0) {
+					triplets.push_back({nums[i], nums[l], nums[r]});
+					while (l < r && nums[l] == nums[l + 1])
+						l++;
+					while (l < r && nums[r] == nums[r - 1])
+						r--;
+					l++;
+				}
+				else if (sum < 0)
+					l++;
+				else
+					r--;
+			}
+		}
+		
+		return triplets;
+	}
 };
