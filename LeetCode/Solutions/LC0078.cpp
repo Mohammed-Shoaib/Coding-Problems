9856b3033ca3df696c339453446a392168868486
Mohammed-Shoaib <shoaib98libra@gmail.com>
2020-07-11 15:06:35 +0400

    [LC] Update Subsets

--- LeetCode/Solutions/LC0078.cpp
+++ LeetCode/Solutions/LC0078.cpp
@@ -1,20 +1,21 @@
 /*
 Problem Statement: https://leetcode.com/problems/subsets/
+Time: O(n • 2ⁿ)
+Space: O(n • 2ⁿ)
+Author: Mohammed Shoaib, github.com/Mohammed-Shoaib
 */
 
 class Solution {
 public:
-    vector< vector<int> > subsets(vector<int>& nums) {
-        int n, size;
-        n = nums.size();
-        size = 1 << n;
-        vector< vector<int> > sets(size);
+	vector<vector<int>> subsets(vector<int>& nums) {
+		int n = nums.size();
+		vector<vector<int>> sets(1 << n);
 
-        for (int i = 0; i < size; i++)
-            for (int j = 0; j < n; j++)
-                if (i & (1 << j))
-                    sets[i].push_back(nums[j]);
-        
-        return sets;
-    }
+		for (int b = 0; b < sets.size(); b++)
+			for (int i = 0; i < n; i++)
+				if ((b >> i) & 1)
+					sets[b].push_back(nums[i]);
+		
+		return sets;
+	}
 };
