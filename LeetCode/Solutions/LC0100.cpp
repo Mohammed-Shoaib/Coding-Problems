76835ec909135f1dc8f764120b503b12eb01efb7
Mohammed-Shoaib <shoaib98libra@gmail.com>
2020-07-13 13:26:27 +0400

    [LC] Update Same Tree

--- LeetCode/Solutions/LC0100.cpp
+++ LeetCode/Solutions/LC0100.cpp
@@ -1,15 +1,16 @@
 /*
 Problem Statement: https://leetcode.com/problems/same-tree/
+Time: O(n)
+Space: O(n)
+Author: Mohammed Shoaib, github.com/Mohammed-Shoaib
 */
 
 class Solution {
 public:
-    bool isSameTree(TreeNode* p, TreeNode* q) {
-        if (!p && !q)
-            return true;
-        else if (!p || !q || p->val != q->val)
-            return false;
-        else
-            return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
-    }
+	bool isSameTree(TreeNode* p, TreeNode* q) {
+		if (!p || !q)
+			return !p && !q;
+		else
+			return p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+	}
 };
