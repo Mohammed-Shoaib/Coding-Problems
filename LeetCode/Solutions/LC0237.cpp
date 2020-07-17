95d732452cb64604835a05b58fa71a97ace04db2
Mohammed-Shoaib <shoaib98libra@gmail.com>
2020-07-17 10:36:28 +0400

    [LC] Update Delete Node in a Linked List with time and space complexities

--- LeetCode/Solutions/LC0237.cpp
+++ LeetCode/Solutions/LC0237.cpp
@@ -1,5 +1,8 @@
 /*
 Problem Statement: https://leetcode.com/problems/delete-node-in-a-linked-list/
+Time: O(1)
+Space: O(1)
+Author: Mohammed Shoaib, github.com/Mohammed-Shoaib
 */
 
 class Solution {
