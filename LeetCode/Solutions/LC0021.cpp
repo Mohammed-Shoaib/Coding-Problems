c2d8967eec60b84730d36a290496d8285bd9c7ec
Mohammed-Shoaib <shoaib98libra@gmail.com>
2020-07-17 10:37:48 +0400

    [LC] Update Merge Two Sorted Lists with time and space complexities

--- LeetCode/Solutions/LC0021.cpp
+++ LeetCode/Solutions/LC0021.cpp
@@ -1,26 +1,32 @@
 /*
 Problem Statement: https://leetcode.com/problems/merge-two-sorted-lists/
+Time: O(m + n)
+Space: O(1)
+Author: Mohammed Shoaib, github.com/Mohammed-Shoaib
 */
 
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        ListNode *head, *l;
-        head = new ListNode(0);
-        l = head;
+	ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+		ListNode *dummy, *l;
+		dummy = new ListNode(0);
+		l = dummy;
 
-        while (l1 && l2) {
-            if (l1->val < l2->val) {
-                l->next = l1;
-                l1 = l1->next;
-            } else {
-                l->next = l2;
-                l2 = l2->next;
-            }
-            l = l->next;
-        }
+		while (l1 && l2) {
+			if (l1->val < l2->val) {
+				l->next = l1;
+				l1 = l1->next;
+			} else {
+				l->next = l2;
+				l2 = l2->next;
+			}
+			l = l->next;
+		}
 
-        l->next = (l1) ? l1 : l2;
-        return head->next;
-    }
+		l->next = (l1) ? l1 : l2;
+		l = dummy->next;
+		delete dummy;
+		
+		return l;
+	}
 };
