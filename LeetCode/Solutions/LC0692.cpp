04992476afb961be6770c80f91640ef9844d9467
Mohammed-Shoaib <shoaib98libra@gmail.com>
2020-07-18 20:21:25 +0400

    [LC] Update Top K Frequent Words

--- LeetCode/Solutions/LC0692.cpp
+++ LeetCode/Solutions/LC0692.cpp
@@ -1,32 +1,37 @@
 /*
 Problem Statement: https://leetcode.com/problems/top-k-frequent-words/
+Time: O(n + k • log k)
+Space: O(n)
+Author: Mohammed Shoaib, github.com/Mohammed-Shoaib
 */
 
 class Solution {
 public:
-    vector<string> topKFrequent(vector<string>& words, int k) {
-		vector<string> ele;
+	vector<string> topKFrequent(vector<string>& words, int k) {
+		vector<string> res(k);
 		unordered_map<string, int> freq;
 
-		// Count occurrences of each word
+		// helper function to compare words
+		auto compare = [](auto& l, auto& r) {
+			if (l.second == r.second)
+				return l.first < r.first;
+			else
+				return l.second > r.second;
+		};
+
+		// count occurrences of each word
 		for (string& word: words)
 			freq[word]++;
-		vector< pair<string, int> > v(freq.begin(), freq.end());
+		vector<pair<string, int>> v(freq.begin(), freq.end());
 		
-		// Sort first k elements by count in decreasing order
+		// sort first k elements by count in decreasing order
 		nth_element(v.begin(), v.begin() + k - 1, v.end(), compare);
 		sort(v.begin(), v.begin() + k, compare);
 
-		// Use sorted vector to create vector of k elements
-		transform(v.begin(), v.begin() + k, back_inserter(ele), [](auto& p) { return p.first; });
+		// get top k elements using sorted vector
+		for (int i = 0; i < k; i++)
+			res[i] = move(v[i].first);
 		
-		return ele;
-    }
-
-	static bool compare(pair<string, int>& l, pair<string, int>& r) {
-		if (l.second == r.second)
-				return l.first < r.first;
-		else
-			return l.second > r.second;
+		return res;
 	}
 };
