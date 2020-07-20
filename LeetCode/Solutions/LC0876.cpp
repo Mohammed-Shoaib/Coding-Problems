2360806a69cb0889334d87f8893ab38733685658
Mohammed-Shoaib <shoaib98libra@gmail.com>
2020-07-20 15:16:07 +0400

    [LC] Update Middle of the Linked List with time and space complexities

--- LeetCode/Solutions/LC0876.cpp
+++ LeetCode/Solutions/LC0876.cpp
@@ -1,16 +1,22 @@
 /*
 Problem Statement: https://leetcode.com/problems/middle-of-the-linked-list/
+Time: O(n)
+Space: O(1)
+Author: Mohammed Shoaib, github.com/Mohammed-Shoaib
 */
 
 class Solution {
 public:
-    ListNode* middleNode(ListNode* head) {
-        ListNode *slow, *fast;
-        slow = fast = head;
-        while (fast && fast->next) {
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-        return slow;
-    }
+	ListNode* middleNode(ListNode* head) {
+		ListNode *slow, *fast;
+		slow = fast = head;
+
+		// floyd's cycle-finding algorithm
+		while (fast && fast->next) {
+			slow = slow->next;
+			fast = fast->next->next;
+		}
+
+		return slow;
+	}
 };
