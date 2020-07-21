66c41573c4dfa790883c4008e00094934780e9f9
Mohammed-Shoaib <shoaib98libra@gmail.com>
2020-07-21 20:59:42 +0400

    [LC] Update Missing Number

--- LeetCode/Solutions/LC0268.cpp
+++ LeetCode/Solutions/LC0268.cpp
@@ -1,12 +1,16 @@
 /*
 Problem Statement: https://leetcode.com/problems/missing-number/
+Time: O(n)
+Space: O(1)
+Author: Mohammed Shoaib, github.com/Mohammed-Shoaib
 */
 
 class Solution {
 public:
-    int missingNumber(vector<int>& nums) {
-        int sum = nums.size() * (nums.size() + 1);
-		sum /= 2;
-		return sum - accumulate(nums.begin(), nums.end(), 0);
-    }
+	int missingNumber(vector<int>& nums) {
+		int b = nums.size();
+		for (int i = 0; i < nums.size(); i++)
+			b ^= i ^ nums[i];
+		return b;
+	}
 };
