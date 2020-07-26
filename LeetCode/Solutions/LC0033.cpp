5ce1404ebc05e9e1641c426885576adc35ee27fd
Mohammed-Shoaib <shoaib98libra@gmail.com>
2020-07-26 22:16:36 +0400

    [LC] Update Search in Rotated Sorted Array

--- LeetCode/Solutions/LC0033.cpp
+++ LeetCode/Solutions/LC0033.cpp
@@ -1,22 +1,26 @@
 /*
 Problem Statement: https://leetcode.com/problems/search-in-rotated-sorted-array/
+Time: O(log n)
+Space: O(1)
+Author: Mohammed Shoaib, github.com/Mohammed-Shoaib
 */
 
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int low, mid, high;
-        low = 0;
-        high = nums.size() - 1;
-        
-        while (low < high) {
-            mid = (low + high) / 2;
-            if ((nums[0] > target) ^ (target > nums[mid]) ^ (nums[0] > nums[mid]))
-                low = mid + 1;
-            else
-                high = mid;
-        }
-
-        return (!nums.empty() && nums[high] == target) ? high : -1;
-    }
+	int search(vector<int>& nums, int target) {
+		int low, mid, high;
+		low = 0;
+		high = (int) nums.size() - 1;
+		
+		// binary search
+		while (low < high) {
+			mid = low + (high - low) / 2;
+			if ((nums[0] > target) ^ (target > nums[mid]) ^ (nums[0] > nums[mid]))
+				low = mid + 1;
+			else
+				high = mid;
+		}
+		
+		return (!nums.empty() && nums[high] == target) ? high : -1;
+	}
 };
