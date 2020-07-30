f6576a19da26c1e7a304e0b08b02ff5a64f21739
Mohammed-Shoaib <shoaib98libra@gmail.com>
2020-07-30 20:02:49 +0400

    [LC] Update Best Time to Buy and Sell Stock with time and space complexities

--- LeetCode/Solutions/LC0121.cpp
+++ LeetCode/Solutions/LC0121.cpp
@@ -1,15 +1,18 @@
 /*
 Problem Statement: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+Time: O(n)
+Space: O(1)
+Author: Mohammed Shoaib, github.com/Mohammed-Shoaib
 */
 
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int profit = 0, min_p = numeric_limits<int>::max();
-		for (int price: prices) {
+	int maxProfit(vector<int>& prices) {
+		int profit = 0, min_p = numeric_limits<int>::max();
+		for (int& price: prices) {
 			min_p = min(price, min_p);
 			profit = max(price - min_p, profit);
 		}
 		return profit;
-    }
+	}
 };
