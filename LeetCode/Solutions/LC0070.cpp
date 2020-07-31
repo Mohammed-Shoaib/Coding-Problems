2f03fcc4cd89d816f13f2ced0cb6cf840e527694
Mohammed-Shoaib <shoaib98libra@gmail.com>
2020-07-31 18:37:19 +0400

    [LC] Update Climbing Stairs

--- LeetCode/Solutions/LC0070.cpp
+++ LeetCode/Solutions/LC0070.cpp
@@ -1,18 +1,16 @@
 /*
 Problem Statement: https://leetcode.com/problems/climbing-stairs/
+Time: O(n)
+Space: O(1)
+Author: Mohammed Shoaib, github.com/Mohammed-Shoaib
 */
 
 class Solution {
 public:
-    int climbStairs(int n) {
-        int f0, f1, f2;
-        f0 = 0;
-        f1 = 1;
-        while (n--) {
-            f2 = f0 + f1;
-            f0 = f1;
-            f1 = f2;
-        }
-        return f2;
-    }
+	int climbStairs(int n) {
+		int f0 = 0, f1 = 1;
+		while (n--)
+			f0 = exchange(f1, f0 + f1);
+		return f1;
+	}
 };
