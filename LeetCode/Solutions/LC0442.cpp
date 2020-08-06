27d1920cf0ae5bff86f790c3e68b13fc66e53eb4
Mohammed-Shoaib <shoaib98libra@gmail.com>
2020-08-06 11:40:29 +0400

    [LC] Update Find All Duplicates in an Array

--- LeetCode/Solutions/LC0442.cpp
+++ LeetCode/Solutions/LC0442.cpp
@@ -1,19 +1,26 @@
 /*
 Problem Statement: https://leetcode.com/problems/find-all-duplicates-in-an-array/
+Time: O(n)
+Space: O(1)
+Author: Mohammed Shoaib, github.com/Mohammed-Shoaib
 */
 
 class Solution {
 public:
-    vector<int> findDuplicates(vector<int>& nums) {
-        int pos;
-        vector<int> res;
-        for (int i = 0; i < nums.size(); i++) {
-            pos = abs(nums[i]) - 1;
-            if (nums[pos] < 0)
-                res.push_back(pos + 1);
-            else
-                nums[pos] *= -1;
-        }
-        return res;
-    }
+	vector<int> findDuplicates(vector<int>& nums) {
+		int n = nums.size();
+		vector<int> dups;
+		
+		for (int i = 0; i < n; i++) {
+			int x = abs(nums[i]) - 1;
+			if (nums[x] < 0)
+				dups.push_back(x + 1);
+			nums[x] *= -1;
+		}
+		for (int& num: nums)
+			if (num < 0)
+				num *= -1;
+		
+		return dups;
+	}
 };
