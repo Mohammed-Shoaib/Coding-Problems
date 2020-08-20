3a4640b00140a7d5a707e91987f3b9ae81b023bc
Mohammed-Shoaib <shoaib98libra@gmail.com>
2020-08-20 06:55:04 +0400

    [LC] Update Reverse Integer

--- LeetCode/Solutions/LC0007.cpp
+++ LeetCode/Solutions/LC0007.cpp
@@ -1,18 +1,24 @@
 /*
 Problem Statement: https://leetcode.com/problems/reverse-integer/
+Time: O(n)
+Space: O(1)
+Author: Mohammed Shoaib, github.com/Mohammed-Shoaib
 */
 
 class Solution {
+private:
+	const int pos_inf = numeric_limits<int>::max();
+	const int neg_inf = numeric_limits<int>::min();
 public:
-    int reverse(int x) {
-        long long rev = 0;
-        while (x != 0) {
-        	rev *= 10;
-        	rev += x % 10;
-        	x /= 10;
-        }
-        if (rev < INT_MIN || rev > INT_MAX)
-        	rev = 0;
-        return rev;
-    }
+	int reverse(int x) {
+		int rev = 0;
+		while (x) {
+			// check overflow
+			if (rev > pos_inf / 10 || rev < neg_inf / 10)
+				return 0;
+			rev = 10 * rev + x % 10;
+			x /= 10;
+		}
+		return rev;
+	}
 };
