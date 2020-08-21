533b5d50b6e7b5e3d2d801b47570ba04b029885a
Mohammed-Shoaib <shoaib98libra@gmail.com>
2020-08-21 13:22:42 +0400

    [LC] Update Subarray Sum Equals K

--- LeetCode/Solutions/LC0560.cpp
+++ LeetCode/Solutions/LC0560.cpp
@@ -1,19 +1,25 @@
 /*
 Problem Statement: https://leetcode.com/problems/subarray-sum-equals-k/
+Time: O(n)
+Space: O(n)
+Author: Mohammed Shoaib, github.com/Mohammed-Shoaib
 */
 
 class Solution {
 public:
-    int subarraySum(vector<int>& nums, int k) {
-		int cnt, sum;
+	int subarraySum(vector<int>& nums, int k) {
+		int cnt, sum, n;
 		unordered_map<int, int> freq;
-		freq[0]++;
 		cnt = sum = 0;
-		for (int i = 0; i < nums.size(); i++) {
+		n = nums.size();
+		freq[0]++;
+		
+		for (int i = 0; i < n; i++) {
 			sum += nums[i];
-			cnt += m[sum - k];
-			m[sum]++;
+			cnt += freq[sum - k];
+			freq[sum]++;
 		}
+		
 		return cnt;
-    }
+	}
 };
