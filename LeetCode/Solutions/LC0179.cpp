56288112d040006550c8197d89b6f1acf3bc4543
Mohammed-Shoaib <shoaib98libra@gmail.com>
2020-09-25 14:31:01 +0400

    [LC] Update Largest Number

--- LeetCode/Solutions/LC0179.cpp
+++ LeetCode/Solutions/LC0179.cpp
@@ -1,39 +1,42 @@
 /*
 Problem Statement: https://leetcode.com/problems/largest-number/
+Time: O(n • log n)
+Space: O(n)
+Author: Mohammed Shoaib, github.com/Mohammed-Shoaib
 */
 
 class Solution {
-public:
-	string largestNumber(vector<int>& nums) {
-		string s;
-		sort(nums.rbegin(), nums.rend(), compare);
-		if (nums.size() && nums[0] == 0)
-			return "0";
-		for (int num: nums)
-			s += to_string(num);
-		return s;
+private:
+	static int64_t convert(vector<int> nums) {
+		int64_t num = 0;
+		for (int x: nums) {
+			int copy = x;
+			while (copy) {
+				num *= 10;
+				copy /= 10;
+			}
+			num += x;
+		}
+		return num;
 	}
 	
 	static bool compare(int l, int r) {
+		int64_t lr, rl;
 		if (l == 0 || r == 0)
-			return l < r;
-		
-		int temp = r;
-		long long ll = l, rr = r;
-		
-		while (temp) {
-			ll *= 10;
-			temp /= 10;
-		}
-		ll += r;
-		
-		temp = l;
-		while (temp) {
-			rr *= 10;
-			temp /= 10;
-		}
-		rr += l;
-		
-		return ll < rr;
+			return l > r;
+		lr = convert({l, r});
+		rl = convert({r, l});
+		return lr > rl;
+	}
+
+public:
+	string largestNumber(vector<int>& nums) {
+		string s;
+		sort(nums.begin(), nums.end(), compare);
+		if (!nums.empty() && nums[0] == 0)
+			return "0";
+		for (int& x: nums)
+			s += to_string(x);
+		return s;
 	}
 };
