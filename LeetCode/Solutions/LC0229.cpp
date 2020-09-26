02f4cb999aae7bbd266d757e0d1190cef9ffc82b
Mohammed-Shoaib <shoaib98libra@gmail.com>
2020-09-26 05:11:05 +0400

    [LC] Update Majority Element II

--- LeetCode/Solutions/LC0229.cpp
+++ LeetCode/Solutions/LC0229.cpp
@@ -1,35 +1,43 @@
 /*
 Problem Statement: https://leetcode.com/problems/majority-element-ii/
+Time: O(n)
+Space: O(1)
+Author: Mohammed Shoaib, github.com/Mohammed-Shoaib
 */
 
 class Solution {
 public:
 	vector<int> majorityElement(vector<int>& nums) {
-		// generalized solution for n / k
-		int k = 3;
+		int n, m1, m2, cnt1, cnt2;
 		vector<int> m;
-		unordered_map<int, int> counters;
+		n = nums.size();
+		cnt1 = cnt2 = 0;
 		
-		// maintain k counters
-		for (auto num: nums) {
-			if (counters.size() < k - 1 || counters.count(num))
-				counters[num]++;
-			else {
-				auto it = counters.begin();
-				while (it != counters.end()) {
-					it->second--;
-					if (it->second == 0)
-						it = counters.erase(it);
-					else
-						it++;
-				}
+		// Boyer–Moore majority vote
+		for (int& x: nums) {
+			if (m1 == x)
+				cnt1++;
+			else if (m2 == x)
+				cnt2++;
+			else if (cnt1 == 0) {
+				m1 = x;
+				cnt1++;
+			} else if (cnt2 == 0) {
+				m2 = x;
+				cnt2++;
+			} else {
+				cnt1--;
+				cnt2--;
 			}
 		}
 		
-		// check if element occurs more than n / k times
-		for (auto it: counters)
-			if (count(nums.begin(), nums.end(), it.first) > nums.size() / k)
-				m.push_back(it.first);
+		cnt1 = count(nums.begin(), nums.end(), m1);
+		cnt2 = count(nums.begin(), nums.end(), m2);
+		
+		if (cnt1 > n / 3)
+			m.push_back(m1);
+		if (cnt2 > n / 3)
+			m.push_back(m2);
 		
 		return m;
 	}
