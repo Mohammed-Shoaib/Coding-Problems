6f38a40077c2adccdc0c9cece200165c2a41aeff
Mohammed-Shoaib <shoaib98libra@gmail.com>
2021-02-20 10:27:49 +0100

    [LC] Update Minimum Remove to Make Valid Parentheses

--- LeetCode/Solutions/LC1249.cpp
+++ LeetCode/Solutions/LC1249.cpp
@@ -1,35 +1,33 @@
 /*
 Problem Statement: https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/
+Time: O(n)
+Space: O(n)
+Author: Mohammed Shoaib, github.com/Mohammed-Shoaib
 */
 
 class Solution {
 public:
-	string minRemoveToMakeValid(string& s) {
-		int n = s.length();
-		string valid;
-		stack<int> st;
-		vector<bool> mark(n);
+	string minRemoveToMakeValid(string s) {
+		int bal, close, pos;
+		bal = close = pos = 0;
 		
-		for (int i = 0; i < n; i++) {
-			if (s[i] == '(')
-				st.push(i);
-			else if (s[i] == ')') {
-				if (st.empty())
-					mark[i] = true;
-				else
-					st.pop();
-			}
+		for (char& c: s)
+			if (c == ')')
+				close++;
+		
+		for (char& c: s) {
+			if (c == ')')
+				close--;
+			if (c == '(' && bal == close || c == ')' && bal == 0)
+				continue;
+			else if (c == '(')
+				bal++;
+			else if (c == ')')
+				bal--;
+			s[pos++] = c;
 		}
+		s.resize(pos);
 		
-		while (!st.empty()) {
-			mark[st.top()] = true;
-			st.pop();
-		}
-		
-		for (int i = 0; i < n; i++)
-			if (!mark[i])
-				valid += s[i];
-		
-		return valid;
+		return s;
 	}
 };
