ae317629ad0ec93304c1504b84ac0cd665c7b494
Mohammed-Shoaib <shoaib98libra@gmail.com>
2021-03-17 07:35:11 +0100

    [LC] Update Flatten Binary Tree to Linked List

--- LeetCode/Solutions/LC0114.cpp
+++ LeetCode/Solutions/LC0114.cpp
@@ -1,23 +1,22 @@
 /*
 Problem Statement: https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
 Time: O(n)
-Space: O(n)
+Space: O(1)
 Author: Mohammed Shoaib, github.com/Mohammed-Shoaib
 */
 
 class Solution {
 public:
 	void flatten(TreeNode* root) {
-		flatten(root, nullptr);
-	}
-	
-	TreeNode* flatten(TreeNode* root, TreeNode* prev) {
-		if (!root)
-			return prev;
-		prev = flatten(root->right, prev);
-		prev = flatten(root->left, prev);
-		root->right = prev;
-		root->left = nullptr;
-		return root;
+		while (root) {
+			TreeNode* node = root->left;
+			if (node) {
+				while (node->right)
+					node = node->right;
+				node->right = root->right;
+				root->right = exchange(root->left, nullptr);
+			}
+			root = root->right;
+		}
 	}
 };
