bb5716097f9e32f4af10e6c410994e99bdf65639
Mohammed-Shoaib <shoaib98libra@gmail.com>
2021-03-24 08:13:21 +0100

    [LC] Update Design Underground System with time and space complexities

--- LeetCode/Solutions/LC1396.cpp
+++ LeetCode/Solutions/LC1396.cpp
@@ -1,11 +1,27 @@
 /*
 Problem Statement: https://leetcode.com/problems/design-underground-system/
+Space: O(n² • len)
+Author: Mohammed Shoaib, github.com/Mohammed-Shoaib
+
+|------------------------------------------|--------|--------|
+| Operations                               | Time   | Space  |
+|------------------------------------------|--------|--------|
+| UndergroundSystem()                      | O(1)   | O(1)   |
+| checkIn(id, stationName, t)              | O(len) | O(len) |
+| checkOut(id, stationName, t)             | O(len) | O(len) |
+| getAverageTime(startStation, endStation) | O(len) | O(len) |
+|------------------------------------------|--------|--------|
 */
 
 class UndergroundSystem {
 private:
+	unordered_map<string, pair<int, int>> adj;
 	unordered_map<int, pair<string, int>> cust;
-	unordered_map<string, pair<double, int>> adj;
+	
+	string convert(string& s1, string& s2) {
+		return s1 + "|" + s2;
+	}
+	
 public:
 	UndergroundSystem() {}
 	
@@ -13,15 +29,15 @@ public:
 		cust[id] = {stationName, t};
 	}
 	
-	void checkOut(int id, string s2, int t2) {
-		string s = cust[id].first + " " + s2;
-		adj[s].first += t2 - cust[id].second;
-		adj[s].second++;
-		cust.erase(id);
+	void checkOut(int id, string stationName, int t) {
+		auto& [startStation, startTime] = cust[id];
+		pair<int, int>& p = adj[convert(startStation, stationName)];
+		p.first += t - startTime;
+		p.second++;
 	}
 	
 	double getAverageTime(string startStation, string endStation) {
-		string s = startStation + " " + endStation;
-		return adj[s].first / adj[s].second;
+		pair<int, int>& p = adj[convert(startStation, endStation)];
+		return (double) p.first / p.second;
 	}
 };
