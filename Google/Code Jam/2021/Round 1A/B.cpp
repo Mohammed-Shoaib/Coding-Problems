dd10cb41b2770b5a3c9798ed9fe011f0a13b1574
Mohammed-Shoaib <shoaib98libra@gmail.com>
2021-04-16 09:51:01 +0200

    [Google] Solve Prime Time

--- Google/Code Jam/2021/Round 1A/B.cpp
+++ Google/Code Jam/2021/Round 1A/B.cpp
@@ -1,26 +1,25 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
 
 using namespace std;
 
-int prime_time(int M, vector<int>& P, vector<int>& N) {
-	int score, max_sum;
+int64_t prime_time(int M, vector<int64_t>& P, vector<int64_t>& N) {
+	int64_t score, max_sum, limit;
 	score = max_sum = 0;
+	limit = 30'000;
 	
 	for (int i = 0; i < M; i++)
 		max_sum += P[i] * N[i];
 	
-	for (int x = 2; x <= max_sum; x++) {
-		int prod = 1, sum = max_sum, num = x;
+	for (int x = 2; x < max_sum && x <= limit; x++) {
+		int64_t sum = x, prod = max_sum - x;
 		for (int i = 0; i < M; i++)
-			for (int j = 0; j < N[i] && num % P[i] == 0; j++) {
+			for (int j = 0; j < N[i] && prod % P[i] == 0; j++) {
 				sum -= P[i];
-				num /= P[i];
-				prod *= P[i];
+				prod /= P[i];
 			}
-		if (sum == prod)
-			score = max(score, sum);
+		if (sum == 0 && prod == 1)
+			score = max(score, max_sum - x);
 	}
 	
 	return score;
@@ -32,7 +31,7 @@ int main() {
 	for (int x = 1; x <= T; x++) {
 		int M;
 		cin >> M;
-		vector<int> P(M), N(M);
+		vector<int64_t> P(M), N(M);
 		for (int i = 0; i < M; i++)
 			cin >> P[i] >> N[i];
 		cout << "Case #" << x << ": " << prime_time(M, P, N) << endl;
