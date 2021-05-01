30d4568f16017e54f3bab842b5df75b886f08dd6
Mohammed-Shoaib <shoaib98libra@gmail.com>
2021-05-02 00:15:39 +0200

    [LC] Update Prefix and Suffix Search to use a paired trie implementation

--- LeetCode/Solutions/LC0745.cpp
+++ LeetCode/Solutions/LC0745.cpp
@@ -3,44 +3,42 @@ Problem Statement: https://leetcode.com/problems/prefix-and-suffix-search/
 Space: O(words • len)
 Author: Mohammed Shoaib, github.com/Mohammed-Shoaib
 
-|-------------------------|--------------------------|--------|
-| Operations              | Time                     | Space  |
-|-------------------------|--------------------------|--------|
-| WordFilter(words)       | O(words • len)           | O(1)   |
-| ends_with(word, suffix) | O(suffix)                | O(1)   |
-| insert(words)           | O(words • len)           | O(1)   |
-| f(prefix, suffix)       | O(prefix + suffix • len) | O(len) |
-|-------------------------|--------------------------|--------|
+Paired Trie Implementation
+|-------------------------|----------------------------------|----------------|
+| Operations              | Time                             | Space          |
+|-------------------------|----------------------------------|----------------|
+| WordFilter(words)       | O(words • len)                   | O(1)           |
+| insert(words)           | O(words • len)                   | O(1)           |
+| f(prefix, suffix)       | O(prefix + suffix + words • len) | O(words • len) |
+|-------------------------|----------------------------------|----------------|
 */
 
 class TrieNode {
 public:
-	int pos = 0;
-	unordered_map<char, TrieNode*> children;
+	int pos = -1;
+	map<pair<char, char>, TrieNode*> children;
 };
 
 class WordFilter {
 private:
 	TrieNode* root;
 	
-	bool ends_with(string& word, string& suffix) {
-		if (suffix.length() > word.length())
-			return false;
-		else
-			return equal(suffix.rbegin(), suffix.rend(), word.rbegin());
-	}
-	
 	void insert(vector<string>& words) {
 		int n = words.size();
 		
 		for (int i = 0; i < n; i++) {
+			int m = words[i].length();
 			TrieNode* node = root;
-			for (char& c: words[i]) {
-				if (!node->children.count(c))
-					node->children[c] = new TrieNode();
-				node = node->children[c];
+			pair<char, char> k;
+			
+			for (int j = 0; j < m; j++) {
+				k = {words[i][j], words[i][m - j - 1]};
+				if (!node->children.count(k))
+					node->children[k] = new TrieNode();
+				node = node->children[k];
 			}
-			node->pos = max(i, node->pos);
+			
+			node->pos = max(i, node->pos);			
 		}
 	}
 
@@ -50,30 +48,49 @@ public:
 	}
 	
 	int f(string prefix, string suffix) {
-		int max_pos = -1;
-		string s = prefix;
+		int max_pos = -1, m = min(prefix.length(), suffix.length());
+		string extra;
+		queue<TrieNode*> q;
+		pair<char, char> k;
 		TrieNode* node = root;
+		reverse(suffix.begin(), suffix.end());
 		
-		// check for prefix
-		for (char& c: prefix) {
-			if (!node->children.count(c))
+		// check for prefix and suffix
+		for (int i = 0; i < m; i++) {
+			k = {prefix[i], suffix[i]};
+			if (!node->children.count(k))
 				return -1;
-			node = node->children[c];
+			node = node->children[k];
 		}
 		
-		// helper function
-		function<void(TrieNode*)> dfs = [&](TrieNode* node) {
-			// check for suffix
-			if (ends_with(s, suffix))
-				max_pos = max(node->pos, max_pos);
-			
-			for (auto& [c, child]: node->children) {
-				s += c;
-				dfs(child);
-				s.pop_back();
+		q.push(node);
+		if (prefix.length() > m)
+			extra = prefix.substr(m);
+		else if (suffix.length() > m)
+			extra = suffix.substr(m);
+		
+		// process the extra prefix or suffix
+		for (int i = 0; i < extra.length(); i++) {
+			int size = q.size();
+			while (size--) {
+				node = q.front();
+				q.pop();
+				for (auto& [k, u]: node->children) {
+					char c = (prefix.length() > m) ? k.first : k.second;
+					if (c == extra[i])
+						q.push(u);
+				}
 			}
-		};
-		dfs(node);
+		}
+		
+		// BFS
+		while (!q.empty()) {
+			node = q.front();
+			q.pop();
+			max_pos = max(node->pos, max_pos);
+			for (auto& [k, u]: node->children)
+				q.push(u);
+		}
 		
 		return max_pos;
 	}
