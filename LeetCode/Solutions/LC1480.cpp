b4c150caa5db2e8de18be488cddf889f815f42c7
Mohammed-Shoaib <shoaib98libra@gmail.com>
2021-05-04 01:07:05 +0200

    [LC] Update Running Sum of 1d Array with time and space complexities

--- LeetCode/Solutions/LC1480.cpp
+++ LeetCode/Solutions/LC1480.cpp
@@ -1,11 +1,15 @@
 /*
 Problem Statement: https://leetcode.com/problems/running-sum-of-1d-array/
+Time: O(n)
+Space: O(n)
+Author: Mohammed Shoaib, github.com/Mohammed-Shoaib
 */
 
 class Solution {
 public:
 	vector<int> runningSum(vector<int> nums) {
-		for (int i = 1; i < nums.size(); i++)
+		int n = nums.size();
+		for (int i = 1; i < n; i++)
 			nums[i] += nums[i - 1];
 		return nums;
 	}
