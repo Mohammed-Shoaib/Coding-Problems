a34de05100264c6fa801b2fda2295280e5b230cb
Mohammed-Shoaib <shoaib98libra@gmail.com>
2021-06-01 18:59:22 +0200

    [LC] Update Shift 2D Grid with time and space complexities

--- LeetCode/Solutions/LC1260.cpp
+++ LeetCode/Solutions/LC1260.cpp
@@ -1,19 +1,23 @@
 /*
 Problem Statement: https://leetcode.com/problems/shift-2d-grid/
+Time: O(m • n)
+Space: O(m • n)
+Author: Mohammed Shoaib, github.com/Mohammed-Shoaib
 */
 
 class Solution {
 public:
-	vector< vector<int> > shiftGrid(vector< vector<int> >& grid, int k) {
-		int size = grid.size() * grid[0].size();
+	vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+		int size, m = grid.size(), n = grid[0].size();
+		size = m * n;
 		k %= size;
 		reverse(0, size - k, grid);
 		reverse(size - k, size, grid);
 		reverse(0, size, grid);
 		return grid;
 	}
-
-	void reverse(int low, int high, vector< vector<int> >& grid) {
+	
+	void reverse(int low, int high, vector<vector<int>>& grid) {
 		int n = grid[0].size();
 		high--;
 		while (low < high) {
