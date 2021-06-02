d39b52b0888d6b2ff7fb8c211244275712721e50
Mohammed-Shoaib <shoaib98libra@gmail.com>
2021-06-02 07:05:02 +0200

    [LC] Update Minimum Time Visiting All Points with time and space complexities

--- LeetCode/Solutions/LC1266.cpp
+++ LeetCode/Solutions/LC1266.cpp
@@ -1,16 +1,19 @@
 /*
 Problem Statement: https://leetcode.com/problems/minimum-time-visiting-all-points/
+Time: O(n)
+Space: O(1)
+Author: Mohammed Shoaib, github.com/Mohammed-Shoaib
 */
 
 class Solution {
 public:
-	int minTimeToVisitAllPoints(vector< vector<int> >& points) {
-		int t = 0;
-		for (int i = 1; i < points.size(); i++)
+	int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+		int t = 0, n = points.size();
+		for (int i = 1; i < n; i++)
 			t += dist(points[i - 1], points[i]);
 		return t;
 	}
-
+	
 	int dist(vector<int>& p1, vector<int>& p2) {
 		int dx, dy;
 		dx = abs(p1[0] - p2[0]);
